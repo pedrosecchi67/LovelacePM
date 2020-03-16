Concluded stability derivative generating functions and fixed mistakes in them. Improved UX functions a little by juggling with the order between force and stability calculations.

aircraft.py
@@ -24,8 +24,8 @@ class aircraft: #class to ease certain case studies for full aircraft
         print('%10s %10s %10s %10s %10s %10s' % ('a', 'b', 'p', 'q', 'r', 'Uinf'))
         print('%10f %10f %10f %10f %10f %10f' % (degrees(self.a), degrees(self.b), self.p, self.q, self.r, self.Uinf))
         print('Geometry parameters:')
-        print('%10s %10s %10s' % ('Sref', 'cref', 'bref'))
-        print('%10f %10f %10f' % (self.Sref, self.cref, self.bref))
+        print('%10s %10s %10s %10s' % ('Sref', 'cref', 'bref', 'AR'))
+        print('%10f %10f %10f %10f' % (self.Sref, self.cref, self.bref, self.AR))
         if self.hascontrol():
             print('Control parameters:')
             controlnames_str=''
@@ -67,6 +67,7 @@ class aircraft: #class to ease certain case studies for full aircraft
             self.bref=b
         else:
             self.bref=bref
+        self.AR=(self.bref**2)/self.Sref
 
         self.a=0.0
         self.b=0.0
@@ -152,15 +153,27 @@ class aircraft: #class to ease certain case studies for full aircraft
             adforces=np.array([self.dCX, self.dCY, self.dCZ])
             self.dCL=adforces@v
             self.dCD=adforces@u
-        
+
         if echo:
             self.forces_report()
+    def freestream_derivative(self, par='a'): #definition of derivative of freestream velocity vectors u and v (normal vectors)
+        if par=='a':
+            du=np.array([-sin(self.a)*cos(self.b), -sin(self.b)*sin(self.a), cos(self.a)])
+            dv=np.array([-cos(self.a)*cos(self.b), -sin(self.b)*cos(self.a), -sin(self.a)])
+        elif par=='b':
+            du=np.array([-cos(self.a)*sin(self.b), cos(self.b)*cos(self.a), 0.0])
+            dv=np.array([sin(self.a)*sin(self.b), -cos(self.b)*sin(self.a), 0.0])
+        else:
+            du=np.zeros(3)
+            dv=np.zeros(3)
+        return du, dv
     def calcstab(self, echo=True):
         #calculate variation on Cps in respect to several parameters
         pars=['a', 'b', 'p', 'q', 'r']
         self.stabderivative_dict={}
+        orforces=np.array([self.CX, self.CY, self.CZ])
         for p in pars:
-            dCps=self.sld.calc_derivative(self.Uinf, a=self.a, b=self.b, p=self.p, q=self.q, r=self.r, par=p)
+            dCps=self.sld.calc_derivative(self.Uinf, a=self.a, b=self.b, p=self.p*2*self.bref*self.Uinf, q=self.q*self.cref*2*self.Uinf, r=self.r*self.bref*2*self.Uinf, par=p)
             forces=[-dCps[i]*self.sld.panels[i].S*self.sld.panels[i].nvector for i in range(self.sld.npanels)]
             moments=[np.cross(self.sld.panels[i].colpoint, forces[i]) for i in range(self.sld.npanels)]
             dCX=sum([forces[i][0] for i in range(self.sld.npanels)])/self.Sref
@@ -169,8 +182,9 @@ class aircraft: #class to ease certain case studies for full aircraft
             u=np.array([cos(self.a)*cos(self.b), sin(self.b)*cos(self.a), sin(self.a)])
             v=np.array([-sin(self.a)*cos(self.b), -sin(self.b)*sin(self.a), cos(self.a)])
             adforces=np.array([dCX, dCY, dCZ])
-            dCL=v@adforces
-            dCD=u@adforces
+            du, dv=self.freestream_derivative(par=p)
+            dCL=v@adforces+orforces@dv
+            dCD=u@adforces+orforces@du
             dCl=sum([moments[i][0] for i in range(self.sld.npanels)])/(self.Sref*self.bref)
             dCm=sum([moments[i][1] for i in range(self.sld.npanels)])/(self.Sref*self.cref)
             dCn=sum([moments[i][2] for i in range(self.sld.npanels)])/(self.Sref*self.bref)
@@ -207,16 +221,23 @@ class aircraft: #class to ease certain case studies for full aircraft
         #reset result readiness
         self.stabavailable=False
         self.forcesavailable=False
+        self.haseqflatplate=False
         self.parameter_report()
     def bodies_eqflatplate_apply(self, rho=1.225, nu=1.72*10e-5, turbulent_criterion=Re2e6, Cf_l_rule=Blausius_Cf_l, Cf_t_rule=Prandtl_1_7th):
+        self.haseqflatplate=True
         for bdy in self.bodies:
             bdy.apply_eqflatplate(rho=rho, nu=nu, turbulent_criterion=turbulent_criterion, Cf_l_rule=Cf_l_rule, Cf_t_rule=Cf_t_rule, Uinf=self.Uinf)
     def eulersolve(self, echo=True, damper=0.0):
-        self.sld.eulersolve(target=np.array([]), a=self.a, b=self.b, p=self.p, q=self.q, r=self.r, damper=damper, Uinf=self.Uinf, echo=True)
+        self.sld.eulersolve(target=np.array([]), a=self.a, b=self.b, p=self.p*self.bref*2*self.Uinf, q=self.q*2*self.cref*self.Uinf, \
+            r=self.r*self.bref*2*self.Uinf, damper=damper, Uinf=self.Uinf, echo=True)
     def forces_report(self):
         print('========Total Forces Report========')
         if self.hascorrections:
-            print('Inviscid case: ')
+            if self.haseqflatplate:
+                str_Cf='+local Cf estimation'
+            else:
+                str_Cf=''
+            print('Inviscid'+str_Cf+' case: ')
         if not self.forcesavailable:
             self.calcforces(echo=False)
         print('%10s %10s %10s %10s %10s %10s | %10s %10s' % ('CX', 'CY', 'CZ', 'Cl', 'Cm', 'Cn', 'CL', 'CD'))

paneller.py
@@ -141,6 +141,7 @@ class Solid:
             te_linind=self.panels[refup].lines.pop(indup)
         else:
             te_linind=self.panels[refup].wakelines.pop(indup-len(self.panels[refup].lines))
+        self.panels[refup].TE_line=te_linind
         if te_linind>0:
             #first wake vortex
             p0=self.lines[te_linind-1, :, 0]
@@ -169,8 +170,9 @@ class Solid:
             indsup=[self.addline(np.vstack((p0, p1)).T, tolerance=tolerance)]
         self.panels[refup].wakelines+=indsup
         if refdown!=-1:
-            self.panels[refdown].lines.pop(indown)
+            te_linind=self.panels[refdown].lines.pop(indown)
             self.panels[refdown].wakelines+=[-l for l in indsup]
+            self.panels[refdown].TE_line=te_linind
     def genwakepanels(self, wakecombs=[], wakeinds=[], offset=1000.0, a=0.0, b=0.0): #generate wake panels based in list of TE panel combinations
         #wakecombs: list of lists, first element in sublist is upper surface panel
         #wakeinds: list of lists indicating corresponding wake vortex line segment index to apply kutta condition to

testcase_sd7062.py
@@ -12,6 +12,7 @@ from utils import *
 from wing import *
 from body import *
 from aircraft import *
+from xfoil_visc import *
 from aerodynamic_output import *
 
 a=radians(0.0)
@@ -27,10 +28,10 @@ sect2=wing_section(afl='sd7062', CA_position=np.array([0.0, b/2, 0.0]), c=croot,
 
 wng1=wing_quadrant(sld, sect1=sect1, sect2=sect2)
 wng=wing(wingquads=[wng1])
-wng.patchcompose(ydisc=40, ystrategy=lambda x: x)#(np.sin(pi*x-pi/2)+1)/2)
 wng.close_tip(sectside=1)
 wng.close_tip(sectside=2)
 acft=aircraft(sld, elems=[wng], Sref=b*croot*(1+taper)/2)
+wng.patchcompose(ydisc=20)
 acft.edit_parameters(par='a', val=a)
 acft.edit_parameters(par='Uinf', val=Uinf)
 acft.addwake()

wing.py
@@ -33,7 +33,7 @@ class wing_section: #class to define wing section based on airfoil info
     def hascorrection(self): #check whether or not external polar is fed to post-processor
         return self.correction!=None
     def getcorrection(self, Re=2e6):
-        self.alphas, self.Cls, self.Cds, self.Cms=self.correction(Re=Re)
+        self.alphas, self.Cls, self.Cds, self.Cms=self.correction(Re=Re, inverse=self.inverted)
     def addcontrols(self, controls=[], control_multipliers=[], control_axpercs=[]):
         if not self.hascontrol():
             self.controls=controls
@@ -61,6 +61,8 @@ class wing_section: #class to define wing section based on airfoil info
         return list(range(inlen, len(self.controls)))
     def hascontrol(self):
         return hasattr(self, 'controls')
+    def hasdeflection(self): #define if there is any deflection in controls in section in question
+        return any([cntrl.DOF.state!=0.0 for cntrl in self.controls])
     def getinthick(self, eta=0.5, x=0.0): #get point in x, at thickness percentage eta
         LE_ind=np.argmin(self.points[:, 0])
         extra=self.points[0:LE_ind+1, :]
@@ -172,7 +174,8 @@ class wing_quadrant: #wing region between two airfoil sections
         #variable including all control DOFs in aircraft object
         #code for prevlines: 'intra_left', 'intra_right', 'extra_...
         #and wing quadrant patch additions
-        controlset=self.acft.controlset
+        if self.acft.hascontrol():
+            controlset=self.acft.controlset
         lspacing=strategy(np.linspace(0.0, 1.0, ldisc))
         extrasld=[]
         xdisc=int(np.size(self.sect1.points, 0)/2)+1
@@ -233,23 +236,25 @@ class wing_quadrant: #wing region between two airfoil sections
         sect2_xdisc=np.size(self.sect2.points, axis=0)
         
         if self.hascontrol():
-            if 'extra_left' in prevlines:
-                for l in range(len(prevlines['extra_left'])):
-                    if l in controlpts1 or l-1 in controlpts1:
-                        prevlines['extra_left'][l]=-2
-            if 'intra_left' in prevlines:
-                for l in range(len(prevlines['intra_left'])):
-                    if sect1_xdisc-1-l in controlpts1 or sect1_xdisc-2-l in controlpts1:
-                        prevlines['intra_left'][l]=-2
-            if 'extra_right' in prevlines:
-                for l in range(len(prevlines['extra_right'])):
-                    if l in controlpts2 or l-1 in controlpts2:
-                        prevlines['extra_right'][l]=-2
-            if 'intra_right' in prevlines:
-                for l in range(len(prevlines['intra_right'])):
-                    if sect2_xdisc-1-l in controlpts2 or sect2_xdisc-2-l in controlpts2:
-                        prevlines['intra_right'][l]=-2
-        
+            if self.sect1.hasdeflection():
+                if 'extra_left' in prevlines:
+                    for l in range(len(prevlines['extra_left'])):
+                        if l in controlpts1 or l-1 in controlpts1:
+                            prevlines['extra_left'][l]=-2
+                if 'intra_left' in prevlines:
+                    for l in range(len(prevlines['intra_left'])):
+                        if sect1_xdisc-1-l in controlpts1 or sect1_xdisc-2-l in controlpts1:
+                            prevlines['intra_left'][l]=-2
+            if self.sect2.hasdeflection():
+                if 'extra_right' in prevlines:
+                    for l in range(len(prevlines['extra_right'])):
+                        if l in controlpts2 or l-1 in controlpts2:
+                            prevlines['extra_right'][l]=-2
+                if 'intra_right' in prevlines:
+                    for l in range(len(prevlines['intra_right'])):
+                        if sect2_xdisc-1-l in controlpts2 or sect2_xdisc-2-l in controlpts2:
+                            prevlines['intra_right'][l]=-2
+            
         prev={}
         if 'extra_right' in prevlines:
             prev['right']=prevlines['extra_right']
@@ -270,6 +275,7 @@ class wing_quadrant: #wing region between two airfoil sections
         LE_lines.reverse()
         TE_lines=[horzlines[0][i] for i in range(len(horzlines[0]))]
         TE_lines.reverse()
+
         prev={'up':LE_lines, 'low':TE_lines}
         if 'intra_right' in prevlines:
             prev['left']=prevlines['intra_right']
@@ -302,22 +308,24 @@ class wing_quadrant: #wing region between two airfoil sections
                 if sect2_xdisc-i-1 in controlpts2:
                     self.intraright_points[i]=self.sect2.points[sect2_xdisc-i-1, :]
             
-            for l in range(len(self.extraleft_lines)):
-                if l in controlpts1 or l-1 in controlpts1:
-                    self.sld.panels[extra_paninds[l][-1]].nocirc_enforce(self.extraleft_lines[l]) #avoid excessive circulation from open control surfaces
-                    self.extraleft_lines[l]=-2
-            for l in range(len(self.intraleft_lines)):
-                if sect1_xdisc-1-l in controlpts1 or sect1_xdisc-2-l in controlpts2:
-                    self.sld.panels[intra_paninds[l][0]].nocirc_enforce(self.intraleft_lines[l]) #avoid excessive circulation from open control surfaces
-                    self.intraleft_lines[l]=-2
-            for l in range(len(self.extraright_lines)):
-                if l in controlpts2 or l-1 in controlpts2:
-                    self.sld.panels[extra_paninds[l][0]].nocirc_enforce(self.extraright_lines[l]) #avoid excessive circulation from open control surfaces
-                    self.extraright_lines[l]=-2
-            for l in range(len(self.intraright_lines)):
-                if sect2_xdisc-1-l in controlpts2 or sect2_xdisc-2-l in controlpts2:
-                    self.sld.panels[intra_paninds[l][-1]].nocirc_enforce(self.intraright_lines[l]) #avoid excessive circulation from open control surfaces
-                    self.intraright_lines[l]=-2
+            if self.sect1.hasdeflection():
+                for l in range(len(self.extraleft_lines)):
+                    if l in controlpts1 or l-1 in controlpts1:
+                        self.sld.panels[extra_paninds[l][-1]].nocirc_enforce(self.extraleft_lines[l]) #avoid excessive circulation from open control surfaces
+                        self.extraleft_lines[l]=-2
+                for l in range(len(self.intraleft_lines)):
+                    if sect1_xdisc-1-l in controlpts1 or sect1_xdisc-2-l in controlpts2:
+                        self.sld.panels[intra_paninds[l][0]].nocirc_enforce(self.intraleft_lines[l]) #avoid excessive circulation from open control surfaces
+                        self.intraleft_lines[l]=-2
+            if self.sect2.hasdeflection():
+                for l in range(len(self.extraright_lines)):
+                    if l in controlpts2 or l-1 in controlpts2:
+                        self.sld.panels[extra_paninds[l][0]].nocirc_enforce(self.extraright_lines[l]) #avoid excessive circulation from open control surfaces
+                        self.extraright_lines[l]=-2
+                for l in range(len(self.intraright_lines)):
+                    if sect2_xdisc-1-l in controlpts2 or sect2_xdisc-2-l in controlpts2:
+                        self.sld.panels[intra_paninds[l][-1]].nocirc_enforce(self.intraright_lines[l]) #avoid excessive circulation from open control surfaces
+                        self.intraright_lines[l]=-2
 
         #wake info
         for i in range(len(TE_extra)):
@@ -417,7 +425,7 @@ class wing_quadrant: #wing region between two airfoil sections
         self.cs=np.array(self.cs)
         self.Gammas=np.array(self.Gammas)
 
-        if self.hascorrection: #apply viscous correction from external polars
+        if self.hascorrection(): #apply viscous correction from external polars
             eta=np.interp(self.ys, np.array([self.sect1.CA_position[perpaxis], self.sect2.CA_position[perpaxis]]), np.array([0.0, 1.0]))
             self.Cls_corrected=np.array([self.sect1.Cls(self.Cls[i])*(1.0-eta[i])+eta[i]*self.sect2.Cls(self.Cls[i]) for i in range(len(eta))])
             self.Cds_corrected=np.array([self.sect1.Cds(self.Cls[i])*(1.0-eta[i])+eta[i]*self.sect2.Cds(self.Cls[i]) for i in range(len(eta))])+self.Cds
@@ -427,9 +435,8 @@ class wing_quadrant: #wing region between two airfoil sections
     def hascorrection(self):
         return self.sect1.hascorrection() and self.sect2.hascorrection()
     def calc_corrected_forces(self): #returns Ceta (for generic parameter eta) considering external polar corrections in coefficients
-        perpvec=np.cross(np.array([1.0, 0.0, 0.0]), self.sect2.CA_position-self.sect1.CA_position)
-        perpvec/=lg.norm(perpvec)
         parvec=self.sect2.CA_position-self.sect1.CA_position
+        parvec[0]=0.0
         parvec/=lg.norm(parvec)
         xposit=np.interp(self.ys, np.array([self.sect1.CA_position[self.perpaxis], self.sect2.CA_position[self.perpaxis]]), \
             np.array([self.sect1.CA_position[0], self.sect2.CA_position[0]]))
@@ -440,15 +447,27 @@ class wing_quadrant: #wing region between two airfoil sections
         dCX=0.0; dCY=0.0; dCZ=0.0; dCl=0.0; dCm=0.0; dCn=0.0
         dmom=np.zeros((len(self.ys), 3))
         dforc=np.zeros((len(self.ys), 3))
+        a=self.acft.a
+        b=self.acft.b
+        p=self.acft.p*self.acft.bref*2
+        q=self.acft.q*self.acft.cref*2
+        r=self.acft.r*self.acft.bref*2
         for i in range(len(self.ys)):
-            dforc[i, :]=perpvec*self.cs[i]*(self.Cls[i]-self.Cls_corrected[i])+np.array([1.0, 0.0, 0.0])*self.cs[i]*(self.Cds[i]-self.Cds_corrected[i])
-            dmom[i, :]=np.cross(np.array([xposit[i], yposit[i], zposit[i]]), dforc[i, :])+(self.cs[i]**2)*(self.Cms[i]-self.Cms_corrected[i])*parvec
-        dCX=np.trapz(dforc[:, 0], x=self.ys)/self.acft.Sref
-        dCY=np.trapz(dforc[:, 1], x=self.ys)/self.acft.Sref
-        dCZ=np.trapz(dforc[:, 2], x=self.ys)/self.acft.Sref
-        dCl=np.trapz(dmom[:, 0], x=self.ys)/(self.acft.Sref*self.acft.bref)
-        dCm=np.trapz(dmom[:, 1], x=self.ys)/(self.acft.Sref*self.acft.cref)
-        dCn=np.trapz(dmom[:, 2], x=self.ys)/(self.acft.Sref*self.acft.bref)
+            posit=np.array([xposit[i], yposit[i], zposit[i]])
+            freestream=np.array([cos(a)*cos(b), -cos(a)*sin(b), sin(a)], dtype='double')+np.cross(np.array([-p, q, -r], dtype='double'), posit)
+            freestream/=lg.norm(freestream)
+            perpvec=self.sect2.CA_position-self.sect1.CA_position
+            perpvec[0]=0.0
+            perpvec=np.cross(freestream, perpvec)
+            perpvec/=lg.norm(perpvec)
+            dforc[i, :]=perpvec*self.cs[i]*(self.Cls_corrected[i]-self.Cls[i])+freestream*self.cs[i]*(self.Cds_corrected[i]-self.Cds[i])
+            dmom[i, :]=np.cross(posit, dforc[i, :])+(self.cs[i]**2)*(self.Cms_corrected[i]-self.Cms[i])*parvec
+        dCX=-np.trapz(dforc[:, 0], x=self.ys)/self.acft.Sref
+        dCY=-np.trapz(dforc[:, 1], x=self.ys)/self.acft.Sref
+        dCZ=-np.trapz(dforc[:, 2], x=self.ys)/self.acft.Sref
+        dCl=-np.trapz(dmom[:, 0], x=self.ys)/(self.acft.Sref*self.acft.bref)
+        dCm=-np.trapz(dmom[:, 1], x=self.ys)/(self.acft.Sref*self.acft.cref)
+        dCn=-np.trapz(dmom[:, 2], x=self.ys)/(self.acft.Sref*self.acft.bref)
         return dCX, dCY, dCZ, dCl, dCm, dCn
 
 class wing:
@@ -590,7 +609,7 @@ class wing:
         for quad in self.wingquads:
             quad.sld.genwakepanels(wakecombs=quad.wakecombs, wakeinds=[[0, 0]], a=a, b=b)
     def hascorrection(self):
-        return any([wngqd.hascorrection() for wngqd in self.wingquads])
+        return all([wngqd.hascorrection() for wngqd in self.wingquads])
     def calc_corrected_forces(self): #compose correction forces from each wing quadrant
         dCX=0.0; dCY=0.0; dCZ=0.0; dCl=0.0; dCm=0.0; dCn=0.0
         for wngqd in self.wingquads:

xfoil_visc.py
@@ -75,7 +75,10 @@ def polar_data(name='n4412', ext_append=True, aseq=[-5.0, 20.0, 1.0], visc=True,
             contentlist=linelist[i].split()
             alphas[i]=float(contentlist[0])
             Cls[i]=float(contentlist[1])
-            Cds[i]=float(contentlist[2])
+            if visc:
+                Cds[i]=float(contentlist[2])
+            else:
+                Cds[i]=float(contentlist[3])
             Cms[i]=float(contentlist[4])
         pfile.close()
         if os.path.exists('temppolar.plr'):
@@ -129,7 +132,6 @@ class polar_correction:
                     str(self.Cds_Re_high[i])+' '+str(self.Cms_Re_high[i])+'\n')
         file.close()
     def create_functions(self, cubic=True):
-        self.alphas_fun=sinterp.CubicSpline(self.Cls_inviscid, self.alphas_inviscid)
         Cls_inviscid_Re_low=np.interp(self.alphas_Re_low, self.alphas_inviscid, self.Cls_inviscid)
         Cls_inviscid_Re_high=np.interp(self.alphas_Re_high, self.alphas_inviscid, self.Cls_inviscid)
         Cds_inviscid_Re_low=np.interp(self.alphas_Re_low, self.alphas_inviscid, self.Cds_inviscid)
@@ -137,6 +139,7 @@ class polar_correction:
         Cms_inviscid_Re_low=np.interp(self.alphas_Re_low, self.alphas_inviscid, self.Cms_inviscid)
         Cms_inviscid_Re_high=np.interp(self.alphas_Re_high, self.alphas_inviscid, self.Cms_inviscid)
         if cubic:
+            self.alphas_fun=sinterp.CubicSpline(self.Cls_inviscid, self.alphas_inviscid)
             self.Cls_Re_low_fun=sinterp.CubicSpline(Cls_inviscid_Re_low, self.Cls_Re_low)
             self.Cls_Re_high_fun=sinterp.CubicSpline(Cls_inviscid_Re_high, self.Cls_Re_high)
             self.Cds_Re_low_fun=sinterp.CubicSpline(Cls_inviscid_Re_low, self.Cds_Re_low-Cds_inviscid_Re_low)
@@ -144,18 +147,25 @@ class polar_correction:
             self.Cms_Re_low_fun=sinterp.CubicSpline(Cls_inviscid_Re_low, self.Cms_Re_low-Cms_inviscid_Re_low)
             self.Cms_Re_high_fun=sinterp.CubicSpline(Cls_inviscid_Re_high, self.Cms_Re_high-Cms_inviscid_Re_high)
         else:
+            self.alphas_fun=sinterp.interp1d(self.Cls_inviscid, self.alphas_inviscid)
             self.Cls_Re_low_fun=sinterp.interp1d(Cls_inviscid_Re_low, self.Cls_Re_low)
             self.Cls_Re_high_fun=sinterp.interp1d(Cls_inviscid_Re_high, self.Cls_Re_high)
             self.Cds_Re_low_fun=sinterp.interp1d(Cls_inviscid_Re_low, self.Cds_Re_low-Cds_inviscid_Re_low)
             self.Cds_Re_high_fun=sinterp.interp1d(Cls_inviscid_Re_high, self.Cds_Re_high-Cds_inviscid_Re_high)
             self.Cms_Re_low_fun=sinterp.interp1d(Cls_inviscid_Re_low, self.Cms_Re_low-Cms_inviscid_Re_low)
             self.Cms_Re_high_fun=sinterp.interp1d(Cls_inviscid_Re_high, self.Cms_Re_high-Cms_inviscid_Re_high)
-    def __call__(self, Re=2e6): #return fitting functions for a given Reynolds number
+    def __call__(self, Re=2e6, inverse=False): #return fitting functions for a given Reynolds number
         eta=np.interp(Re, np.array([self.Re_low, self.Re_high]), np.array([0.0, 1.0]))
-        alphas=lambda Cl: self.alphas_fun(Cl)
-        Cls=lambda Cl: self.Cls_Re_low_fun(Cl)*(1.0-eta)+eta*self.Cls_Re_high_fun(Cl)
-        Cds=lambda Cl: self.Cds_Re_low_fun(Cl)*(1.0-eta)+eta*self.Cds_Re_high_fun(Cl)
-        Cms=lambda Cl: self.Cms_Re_low_fun(Cl)*(1.0-eta)+eta*self.Cms_Re_high_fun(Cl)
+        if not inverse:
+            alphas=lambda Cl: self.alphas_fun(-Cl)
+            Cls=lambda Cl: self.Cls_Re_low_fun(Cl)*(1.0-eta)+eta*self.Cls_Re_high_fun(Cl)
+            Cds=lambda Cl: self.Cds_Re_low_fun(Cl)*(1.0-eta)+eta*self.Cds_Re_high_fun(Cl)
+            Cms=lambda Cl: self.Cms_Re_low_fun(Cl)*(1.0-eta)+eta*self.Cms_Re_high_fun(Cl)
+        else:
+            alphas=lambda Cl: -self.alphas_fun(-Cl)
+            Cls=lambda Cl: -(self.Cls_Re_low_fun(-Cl)*(1.0-eta)+eta*self.Cls_Re_high_fun(-Cl))
+            Cds=lambda Cl: self.Cds_Re_low_fun(-Cl)*(1.0-eta)+eta*self.Cds_Re_high_fun(-Cl)
+            Cms=lambda Cl: -(self.Cms_Re_low_fun(-Cl)*(1.0-eta)+eta*self.Cms_Re_high_fun(-Cl))
         return alphas, Cls, Cds, Cms
 
 def read_polar(poldir='polars', polname='n4412', ext_append=True, echo=True, cubic=True): #read polars from dumped file