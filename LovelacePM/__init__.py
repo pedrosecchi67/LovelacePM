47d8ef33dcca20c4f73846c4989e681946ef0e1c
pedrosecchi67 <pedrosecchimail@gmail.com>
2020-04-25 20:55:27 -0300

    Added new version number and new release note file to be read by loveUpdate and printed to screen when autoupdated

--- LovelacePM/__init__.py
+++ LovelacePM/__init__.py
@@ -69,50 +69,50 @@ if multiprocess_guard():
         print('NOTICE: update registry file not found. Creating anew')
         lupfile=open('updatestat.lup', 'wb')
         statusdict={'lastupdate':tdy, 'updateme':True, 'message':True}
-        _=LoveUpdate.update('LoveUpdate', '')
         pickle.dump(statusdict, lupfile)
         lupfile.close()
+    try:
+        #Read update registry file
+        lupfile=open('updatestat.lup', 'rb')
+        statusdict=pickle.load(lupfile)
+        lupfile.close()
 
-    #Read update registry file
-    lupfile=open('updatestat.lup', 'rb')
-    statusdict=pickle.load(lupfile)
-    lupfile.close()
+        #Declare variables
+        hasupdated=False
+        hasupdated_lup=False
+        code_lup=False
+        code=False
 
-    #Declare variables
-    hasupdated=False
-    hasupdated_lup=False
-    code_lup=False
-    code=False
+        #If an older version update file doesn't include message suppression/enabling variable, create it
+        if not 'message' in statusdict:
+            update_message_enable() #activate message issuing when autoupdate is not performed
+            statusdict['message']=True
 
-    #If an older version update file doesn't include message suppression/enabling variable, create it
-    if not 'message' in statusdict:
-        update_message_enable() #activate message issuing when autoupdate is not performed
-        statusdict['message']=True
-
-    #Update if activated
-    if statusdict['updateme']:
-        code_lup, hasupdated_lup, tdy=LoveUpdate.update('LoveUpdate', statusdict['lastupdate'])
-        if not code_lup:
-            if hasupdated_lup:
-                print('LoveUpdate has autoupdated. If you wish to deactivate the autoupdate feature, import LovelacePM in another python instance and use LovelacePM.updatecancel()')
-            code, hasupdated, tdy=LoveUpdate.update('LovelacePM', statusdict['lastupdate'])
-            code=code and code_lup
-            if not code:
-                if hasupdated:
-                    print('LovelacePM has autoupdated. If you wish to deactivate the autoupdate feature, import LovelacePM in another python instance and use LovelacePM.updatecancel()')
-                statusdict['lastupdate']=tdy
-                lupfile=open('updatestat.lup', 'wb')
-                pickle.dump(statusdict, lupfile)
-                lupfile.close()
+        #Update if activated
+        if statusdict['updateme']:
+            code_lup, hasupdated_lup, tdy=LoveUpdate.update('LoveUpdate', statusdict['lastupdate'])
+            if not code_lup:
+                if hasupdated_lup:
+                    print('LoveUpdate has autoupdated. If you wish to deactivate the autoupdate feature, import LovelacePM in another python instance and use LovelacePM.updatecancel()')
+                code, hasupdated, tdy=LoveUpdate.update('LovelacePM', statusdict['lastupdate'])
+                code=code and code_lup
+                if not code:
+                    if hasupdated:
+                        print('LovelacePM has autoupdated. If you wish to deactivate the autoupdate feature, import LovelacePM in another python instance and use LovelacePM.updatecancel()')
+                    statusdict['lastupdate']=tdy
+                    lupfile=open('updatestat.lup', 'wb')
+                    pickle.dump(statusdict, lupfile)
+                    lupfile.close()
+                else:
+                    print('WARNING: error in autoupdate. Check internet connection if you wish to update LovelacePM. Moving on')
             else:
-                print('WARNING: error in autoupdate. Check internet connection if you wish to update LovelacePM. Moving on')
-        else:
-            print('WARNING: error in autoupdate. Check internet connection if you wish to update LoveUpdate. Moving on')
-    elif statusdict['message']: #warn if message flag is activated
-        print('WARNING: LovelacePM autoupdate is deactivated. if you wish to activate autoupdate, import LovelacePM in another python instance and use LovelacePM.updateset()')
-        print('To suppress this message, use LovelacePM.update_message_suppress(). To undo this action, use LovelacePM.update_message_enable()')
+                print('WARNING: error in autoupdate. Check internet connection if you wish to update LoveUpdate. Moving on')
+        elif statusdict['message']: #warn if message flag is activated
+            print('WARNING: LovelacePM autoupdate is deactivated. if you wish to activate autoupdate, import LovelacePM in another python instance and use LovelacePM.updateset()')
+            print('To suppress this message, use LovelacePM.update_message_suppress(). To undo this action, use LovelacePM.update_message_enable()')
+        del code, code_lup, lupfile, tdy, hasupdated_lup, hasupdated
+    except:
+        print('WARNING: unable to reach LovelacePM update registry. Not autoupdating')
 
-
-    del code, code_lup, lupfile, tdy, hasupdated_lup, hasupdated
 os.chdir(ordir)
 del ordir
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import setuptools
 #from numpy.distutils.core import Extension
 from numpy.distutils.core import setup
+import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+vnumber="0.2.4"
 
 #libraries=[('toolkit', dict(sources=['toolkit.f90']))]
 #extensions=[Extension(name='LovelacePM.toolkit', sources=['LovelacePM/toolkit.f90'], language='f90'), \
@@ -12,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="LovelacePM",
-    version="0.2.3",
+    version=vnumber,
     author="Pedro de Almeida Secchi",
     author_email="pedrosecchimail@gmail.com",
     description="Python based, open source vortex ring panel method code",
@@ -20,7 +22,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pedrosecchi67/LovelacePM",
     packages=['LovelacePM'],
-    package_data={'':['*.dat']},
+    package_data={'':['*.dat', 'VERSION_NOTES.txt']},
     #libraries=libraries,
     #ext_modules=extensions,
     classifiers=[
@@ -31,3 +33,9 @@ setup(
     install_requires=['numpy', 'func-timeout', 'scipy', 'matplotlib', 'cloudpickle', 'pyqtgraph', 'PyOpenGL', 'LoveUpdate'],
     python_requires='>=3.6',
 )
+
+try:
+    import LoveUpdate as lupd
+    lupd.release_note_report(fname='VERSION_NOTES.txt', fdir=os.path.dirname(__file__), program='LovelacePM', version='v'+vnumber)
+except:
+    print('LoveUpdate not yet available for release notes')
